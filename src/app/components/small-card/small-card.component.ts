9811b58371f07eca647b91076992a1b98be2506a
bymauriciodev <mktechnologyweb@gmail.com>
2023-09-08 16:21:51 -0300

    Pronto para mais atualizações

---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrls: ['./small-card.component.css']
+})
+export class SmallCardComponent {
+@Input()
+photoCover:string=""
+@Input()
+cardTitle:string="Saiu a nova versão do Angular"
+@Input()
+Id:string="0"
+}
---
+++ src/app/data/datafake.ts
@@ -0,0 +1,16 @@
+export const dataFake =  [
+  {"id":"1",
+
+  "title": "vamos",
+  "description":"sim",
+  "photoCover":"https://www.fashionbubbles.com/wp-content/uploads/2022/08/60-anos-do-homem-aranha.jpg"
+},
+
+{"id":"2",
+"description":"não",
+"title": "vamos la",
+"photoCover":"https://d2d7ho1ae66ldi.cloudfront.net/ArquivoNoticias/7ec0067a-460f-11ec-aa6e-9587410378a2/marvel-disney-plus-shows.jpg"
+}
+
+
+]
